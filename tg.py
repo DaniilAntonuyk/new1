7aa203c7ea01c83decdc954c730fe095d778e2f9
Pavel_Padal <davaysvoilogi4@mail.ru>
2025-01-20 09:24:50 +0300

    5й принт изменен, должен быть конфликт

--- tg.py
+++ tg.py
@@ -1,4 +1,7 @@
 print ('Hello world')
 print ("Enother one")
+def no_1 ():
+    None
 print ('Branch Feature')
 print ('Четвертый принт для ветки feature')
+print ('5й принт, его я изменил в Feature')
